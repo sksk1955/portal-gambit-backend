485f40f557cafd72891732ec9eaf0d602d2e0a7b
Rudra Garg <rudra.garg22b@iiitg.ac.in>
2025-01-30 18:44:11 +0530

    delete everything

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from app.routes import auth_routes, game_routes
 
 app = FastAPI(title="Portal Gambit")
 
-# Register routers
-app.include_router(auth_routes.router)
-app.include_router(game_routes.router)
 
 if __name__ == '__main__':
     import uvicorn
