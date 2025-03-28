d19754e223446bd0d6fae45caa9e9081b7a251de
Rudra Garg <rudra.garg22b@iiitg.ac.in>
2025-03-28 19:03:10 +0530

    update run

--- run.sh
+++ run.sh
@@ -3,13 +3,8 @@
 # Exit on any error
 set -e
 
-echo "$FIREBASE_CONFIG_STRING"
-echo "$JWT_SECRET"
-echo "$FIREBASE_DATABASE_URL"
-
-
 # Convert Firebase config
-config/convert.sh FIREBASE_CONFIG_STRING config/firebase_service_account.json
+config/convert.sh FIREBASE_CONFIG config/firebase_service_account.json
 
 # Start the application using uvicorn
 exec uvicorn main:app --host 0.0.0.0 --port "${PORT:-8080}"
